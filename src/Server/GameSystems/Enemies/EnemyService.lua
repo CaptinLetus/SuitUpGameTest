fa816cfe73b1dafb396f0bf8f6ae1c08c5dad288
CaptinLetus <53979157+CaptinLetus@users.noreply.github.com>
2022-11-06 11:38:55 -0400

    Create continuous stream of enemies

---
+++ src/Server/GameSystems/Enemies/EnemyService.lua
@@ -0,0 +1,32 @@
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+local Knit = require(ReplicatedStorage.Packages.Knit)
+
+local EnemyService = Knit.CreateService {
+	Name = "EnemyService";
+	Client = {};
+}
+
+
+function EnemyService:KnitStart()
+	task.spawn(function ()
+		while task.wait(5) do
+			self:SpawnEnemy()
+		end
+	end)
+end
+
+
+function EnemyService:KnitInit()
+	
+end
+
+function EnemyService:SpawnEnemy()
+	local newEnemy = ReplicatedStorage.Assets.Enemies.BaconHair:Clone()
+
+	newEnemy.Parent = workspace
+	newEnemy:PivotTo(workspace.Nodes["0"].CFrame)
+end
+
+
+return EnemyService
