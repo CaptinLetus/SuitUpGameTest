78493b9fdfa07862357ea62640eb7749a8ec9297
CaptinLetus <53979157+CaptinLetus@users.noreply.github.com>
2022-11-10 18:07:17 -0400

    Add new enemy type

--- src/Shared/Data/Levels/FirstLevel.lua
+++ src/Shared/Data/Levels/FirstLevel.lua
@@ -1,4 +1,5 @@
 return {
+	-- WAVE 1
 	{
 		enemies = {
 			{
@@ -48,6 +49,15 @@ return {
 	{
 		enemies = {
 			{
+				enemy = "Runner",
+				amount = 1,
+			},
+		},
+		length = 0,
+	},
+	{
+		enemies = {
+			{
 				enemy = "BaconHair",
 				amount = 3,
 			},
@@ -64,5 +74,101 @@ return {
 		},
 		length = 3,
 		loop = 3,
+	},
+	{
+		enemies = {
+			{
+				enemy = "Runner",
+				amount = 2,
+			},
+		},
+		length = 0.2,
+	},
+	{
+		enemies = {
+			{
+				enemy = "BaconHair",
+				amount = 5,
+			},
+		},
+		length = 10,
+	},
+	-- WAVE 2
+	{
+		enemies = {
+			{
+				enemy = "BaconHair",
+				amount = 5,
+			},
+		},
+		length = 3,
+		loop = 3,
+	},
+	{
+		enemies = {
+			{
+				enemy = "Runner",
+				amount = 4,
+			},
+		},
+		length = 0.2,
+	},
+	{
+		enemies = {
+			{
+				enemy = "BaconHair",
+				amount = 6,
+			},
+		},
+		length = 3,
+		loop = 5,
+	},
+	{
+		enemies = {
+			{
+				enemy = "BaconHair",
+				amount = 7,
+			},
+		},
+		length = 4,
+		loop = 3,
+	},
+	{
+		enemies = {
+			{
+				enemy = "Runner",
+				amount = 6,
+			},
+		},
+		length = 0.2,
+	},
+	{
+		enemies = {
+			{
+				enemy = "BaconHair",
+				amount = 8,
+			},
+		},
+		length = 3,
+		loop = 5,
+	},
+	{
+		enemies = {
+			{
+				enemy = "Runner",
+				amount = 6,
+			},
+		},
+		length = 0.2,
+	},
+	{
+		enemies = {
+			{
+				enemy = "BaconHair",
+				amount = 12,
+			},
+		},
+		length = 5,
+		loop = 2,
 	},
 }
